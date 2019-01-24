3aa0adef81b6e98e566475b24dccf360ba905390
fastsystem <info@fast-system.jp>
2019-01-25 07:26:33 +0900

    init

---
+++ Assets/GpuInstancingManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GpuInstancingManager : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public Material material;
+
+	void Start () {
+		
+        for(int x = -10; x < 10; x++)
+        {
+            for (int y = -10; y < 10; y++)
+            {
+                for (int z = -10; z < 10; z++)
+                {
+                    var go = Instantiate(prefab, new Vector3(x, y, z) * 1.2f, Quaternion.identity);
+                    go.GetComponent<MeshRenderer>().sharedMaterial = material;
+                }
+            }
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
